8f3ea68a67f9d8e6fffcec520970ed911f608615
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-14 23:58:47 +0900

    add parser

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,7 +1,7 @@
 module Day02 where
 
-import Data.List
-import Data.Maybe
+-- import Data.List
+-- import Data.Maybe
 
 main :: IO ()
 main = do
@@ -12,8 +12,20 @@ main = do
 --   putStrLn $ "Part 1: " ++ show (part1 parsed)
 --   putStrLn $ "Part 2: " ++ show (part2 parsed)
 
-parse :: String -> String
-parse = id
+parse :: String -> [(Int, Int, Int)]
+parse str = map getTuple $ lines str
+  where
+    getTuple s =
+      let [(a, rest1)] = reads s
+          [(b, rest2)] = reads $ tail rest1
+          [(c, _)] = reads $ tail rest2
+       in (a, b, c)
+
+--   where
+--     getTuple s = (fst $ br s, fst $ br $ rest s, tail $ snd $ br $ rest s)
+--     br s = break (== 'x') s
+--     rest s = tail $ snd $ br s
+--     readTuple (a, b, c) = (read a, read b, read c)
 
 part1 :: String -> Int
 part1 = undefined
