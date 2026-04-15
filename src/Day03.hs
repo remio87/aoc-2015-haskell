b6dd3c50c63724e6ffcf7fcdfa3eab956de4d582
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-15 22:52:58 +0900

    setup day 03

---
+++ src/Day03.hs
@@ -0,0 +1,22 @@
+module Day03 where
+
+-- import Data.List
+-- import Data.Maybe
+
+main :: IO ()
+main = do
+  input <- readFile "inputs/day03.txt"
+  return ()
+
+-- let parsed = parse input
+--   putStrLn $ "Part 1: " ++ show (part1 input)
+--   putStrLn $ "Part 2: " ++ show (part2 input)
+
+parse :: String -> [(Int, Int, Int)]
+parse = undefined
+
+part1 :: String -> Int
+part1 = undefined
+
+part2 :: String -> Int
+part2 = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 import qualified Day01
 import qualified Day02
+import qualified Day03
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -8,4 +9,5 @@ main = do
   case args of
     ["01"] -> Day01.main
     ["02"] -> Day02.main
+    ["03"] -> Day03.main
     _ -> putStrLn "Usage: aoc2015 <day>"
