6884e4d80868dd18ce51e62a156beb07d2506be6
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-15 23:24:29 +0900

    solve day03 part1

--- src/Day03.hs
+++ src/Day03.hs
@@ -3,20 +3,42 @@ module Day03 where
 -- import Data.List
 -- import Data.Maybe
 
+import Control.Monad.Trans.State
+import qualified Control.Monad.Trans.State
+import Data.List (nub)
+
 main :: IO ()
 main = do
   input <- readFile "inputs/day03.txt"
-  return ()
+  putStrLn $ "Part 1: " ++ show (part1 input)
 
--- let parsed = parse input
---   putStrLn $ "Part 1: " ++ show (part1 input)
 --   putStrLn $ "Part 2: " ++ show (part2 input)
 
-parse :: String -> [(Int, Int, Int)]
-parse = undefined
+data Direction = DRight | DLeft | DUp | DDown
+
+type Coord = (Int, Int)
+
+visit :: Direction -> State [Coord] Coord
+visit d = state (\s -> (nextPos (last s) d, s ++ [(nextPos (last s) d)]))
+  where
+    nextPos (x, y) DRight = (x + 1, y)
+    nextPos (x, y) DLeft = (x - 1, y)
+    nextPos (x, y) DUp = (x, y + 1)
+    nextPos (x, y) DDown = (x, y - 1)
+
+parse :: String -> [Direction]
+parse = map charToDir
+  where
+    charToDir '>' = DRight
+    charToDir '<' = DLeft
+    charToDir '^' = DUp
+    charToDir 'v' = DDown
+    charToDir _ = undefined
 
 part1 :: String -> Int
-part1 = undefined
+part1 str = length $ nub visitedCoords
+  where
+    (_, visitedCoords) = runState (mapM_ visit (parse str)) [(0, 0)]
 
 part2 :: String -> Int
 part2 = undefined
