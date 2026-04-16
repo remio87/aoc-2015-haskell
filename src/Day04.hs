851a16654409d68d0bfaf7d180ae7402818f29e4
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-16 23:56:43 +0900

    solve day04 part1

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,14 +1,33 @@
 module Day04 where
 
+import Crypto.Hash (Digest, MD5, hash)
+import Data.ByteString.Char8 (pack)
+import Data.List (isPrefixOf)
+
 main :: IO ()
 main = do
-  return ()
+  putStrLn $ "Part 1: " ++ show (part1 prefix)
 
---   putStrLn $ "Part 1: " ++ show (part1 input)
 --   putStrLn $ "Part 2: " ++ show (part2 input)
 
+prefix = "iwrupvqb"
+
 part1 :: String -> Int
-part1 = undefined
+part1 pf = head $ filter valid [0 ..]
+  where
+    valid :: Int -> Bool
+    valid i = "00000" `isPrefixOf` md5 (pf ++ (show i))
+
+-- check str = isPrefixOf "00000" str
+
+-- checkNum :: String -> Int -> (Bool, Int)
+-- checkNum pf i = (check $ md5 (pf ++ (show i)), i)
+
+md5 :: String -> String
+md5 s = show (hash (pack s) :: Digest MD5)
+
+-- part1 :: String -> Int
+-- part1 pf = snd $ maybe (False, 0) id $ find (\(b, _) -> b) $ map (checkNum pf) [0 ..]
 
 part2 :: String -> Int
 part2 = undefined
