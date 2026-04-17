5f5e8857bb9707c6626c793cfe95685f3ce19889
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-17 22:31:07 +0900

    solve day04 part2

--- src/Day04.hs
+++ src/Day04.hs
@@ -7,8 +7,7 @@ import Data.List (isPrefixOf)
 main :: IO ()
 main = do
   putStrLn $ "Part 1: " ++ show (part1 prefix)
-
---   putStrLn $ "Part 2: " ++ show (part2 input)
+  putStrLn $ "Part 2: " ++ show (part2 prefix)
 
 prefix = "iwrupvqb"
 
@@ -30,4 +29,7 @@ md5 s = show (hash (pack s) :: Digest MD5)
 -- part1 pf = snd $ maybe (False, 0) id $ find (\(b, _) -> b) $ map (checkNum pf) [0 ..]
 
 part2 :: String -> Int
-part2 = undefined
+part2 pf = head $ filter valid [0 ..]
+  where
+    valid :: Int -> Bool
+    valid i = "000000" `isPrefixOf` md5 (pf ++ (show i))
