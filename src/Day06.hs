28d87dc53d2b6e417d8e6a1d8172fdd15b46092e
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-19 00:13:26 +0900

    parser and init array

--- src/Day06.hs
+++ src/Day06.hs
@@ -8,6 +8,34 @@ main = do
 --   putStrLn $ "Part 1: " ++ show (part1 input)
 --   putStrLn $ "Part 2: " ++ show (part2 input)
 
+initArray :: [[Bool]]
+initArray = take 1000 $ repeat (take 1000 $ repeat False)
+
+type Coord = (Int, Int)
+
+type Rect = (Coord, Coord)
+
+data Op = On | Off | Toggle
+  deriving (Show)
+
+parseCoord :: String -> Coord
+parseCoord s =
+  let tup = break (== ',') s
+   in (read $ fst $ tup, read $ tail $ snd tup)
+
+parseOp :: [String] -> (Op, [String])
+parseOp ("turn" : "on" : rest) = (On, rest)
+parseOp ("turn" : "off" : rest) = (Off, rest)
+parseOp ("toggle" : rest) = (Toggle, rest)
+parseOp _ = undefined
+
+parseLine :: String -> (Op, Rect)
+parseLine s =
+  let (op, rest) = parseOp $ words $ s
+   in case rest of
+        (lb : _ : ru : _) -> (op, (parseCoord lb, parseCoord ru))
+        _ -> undefined
+
 part1 :: String -> Int
 part1 = undefined
 
