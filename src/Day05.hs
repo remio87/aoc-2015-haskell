04061fa39a7c148194200e918f332c8dc50a12db
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-18 20:50:00 +0900

    solve day05 part2

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,11 +1,12 @@
 module Day05 where
 
+import qualified Data.Map as Map
+
 main :: IO ()
 main = do
   input <- readFile "inputs/day05.txt"
   putStrLn $ "Part 1: " ++ show (part1 input)
-
---   putStrLn $ "Part 2: " ++ show (part2 prefix)
+  putStrLn $ "Part 2: " ++ show (part2 input)
 
 data NiceState = NiceState
   { vowels :: Int,
@@ -50,5 +51,51 @@ isNice s =
 part1 :: String -> Int
 part1 = length . filter id . map isNice . parse
 
+data NiceState2 = NiceState2
+  { curPos :: Int,
+    prevprev :: Maybe Char,
+    prev :: Maybe Char,
+    pastPairs :: Map.Map String Int,
+    repeatAppeared :: Bool,
+    pairAppeared :: Bool
+  }
+  deriving (Show)
+
+addPair :: Maybe Char -> Char -> Int -> Map.Map String Int -> Map.Map String Int
+addPair Nothing _ _ m = m
+addPair (Just p) c pos m =
+  let pair = [p, c]
+   in case Map.lookup pair m of
+        Nothing -> Map.insert pair pos m
+        (Just _) -> m
+
+checkRepeat :: Maybe Char -> Maybe Char -> Char -> Bool
+checkRepeat Nothing _ _ = False
+checkRepeat _ Nothing _ = False
+checkRepeat (Just pp) (Just _) c = pp == c
+
+checkPair :: Maybe Char -> Char -> Int -> Map.Map String Int -> Bool
+checkPair Nothing _ _ _ = False
+checkPair (Just p) c pos m =
+  let pair = [p, c]
+   in case Map.lookup pair m of
+        Nothing -> False
+        Just pairPos -> pos - pairPos > 1
+
+isNice2 :: String -> Bool
+isNice2 str =
+  let res = foldl step2 (NiceState2 0 Nothing Nothing Map.empty False False) str
+   in repeatAppeared res && pairAppeared res
+
+step2 :: NiceState2 -> Char -> NiceState2
+step2 (NiceState2 cp pp p pairs ra pa) c =
+  let nextPos = cp + 1
+      nextpp = p
+      nextp = Just c
+      nextPairs = addPair p c cp pairs
+      nextRa = ra || checkRepeat pp p c
+      nextPa = pa || checkPair p c cp pairs
+   in NiceState2 nextPos nextpp nextp nextPairs nextRa nextPa
+
 part2 :: String -> Int
-part2 = undefined
+part2 = length . filter id . map isNice2 . parse
