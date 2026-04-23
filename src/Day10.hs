a0f5d7b4b78e53c44a28c70e3cae09019e73f37d
remio87 <82135212+remio87@users.noreply.github.com>
2026-04-23 22:03:32 +0900

    solve day10 part1

--- src/Day10.hs
+++ src/Day10.hs
@@ -2,14 +2,33 @@ module Day10 where
 
 main :: IO ()
 main = do
-  input <- readFile "inputs/day10.txt"
-  return ()
+  putStrLn $ "Part 1: " ++ show (part1 "1113122113")
 
---   putStrLn $ "Part 1: " ++ show (part1 input)
 --   putStrLn $ "Part 2: " ++ show (part2 input)
 
+data Accum = Accum {prev :: Int, count :: Int} deriving (Show)
+
+lookAndSay :: [Accum] -> [Int] -> [Accum]
+lookAndSay acc [] = acc
+lookAndSay [] (i : is) = lookAndSay [Accum i 1] is
+lookAndSay ((Accum p c) : as) (i : is) =
+  if p == i
+    then
+      lookAndSay ((Accum p (c + 1)) : as) is
+    else
+      lookAndSay ((Accum i 1) : (Accum p c) : as) is
+
+accumToResult :: [Accum] -> String
+accumToResult = foldl fn ""
+  where
+    fn :: String -> Accum -> String
+    fn str (Accum val cnt) = show cnt ++ show val ++ str
+
+parseLine :: String -> [Int]
+parseLine = map (read . (: []))
+
 part1 :: String -> Int
-part1 input = undefined
+part1 = length . last . take 41 . iterate (accumToResult . lookAndSay [] . parseLine)
 
 part2 :: String -> Int
-part2 input = undefined
+part2 = undefined
