1eed590177f4b3c34f5ee9345ecd5041f9c7a41d
remio87 <82135212+remio87@users.noreply.github.com>
2026-05-02 13:15:47 +0900

    impl parser

--- src/Day13.hs
+++ src/Day13.hs
@@ -5,6 +5,25 @@ main = do
   input <- readFile "inputs/day13.txt"
   return ()
 
+data Line = Line
+  { sub :: String,
+    obj :: String,
+    happy :: Int
+  }
+  deriving (Show)
+
+parseLine :: String -> Line
+parseLine = go . words
+  where
+    go (s : _ : sign : v : _ : _ : _ : _ : _ : _ : o : _) =
+      let val = read v
+          hap = if sign == "gain" then val else -1 * val
+       in Line s (init o) hap
+    go _ = undefined
+
+parseFile :: String -> [Line]
+parseFile = map parseLine . lines
+
 part1 :: String -> Int
 part1 = undefined
 
