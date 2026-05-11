4a0bde64b0afef61708f5c894f3c3bd468266970
remio87 <82135212+remio87@users.noreply.github.com>
2026-05-11 21:10:09 +0900

    solve day20 part1

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 module Day20 where
 
+import Data.Array (Array, accumArray, assocs)
 import Data.List (nub)
 
 main :: IO ()
@@ -7,24 +10,39 @@ main = do
   return ()
 
 input :: Int
-input = 36000000
+input = 3_600_000
 
-allFactors :: Int -> [Int]
-allFactors i = nub $ firstHalf ++ lastHalf
-  where
-    isqrt = floor . sqrt . fromIntegral
-    firstHalf = filter (\divisor -> i `mod` divisor == 0) [1 .. isqrt i]
-    lastHalf = map (\divisor -> i `div` divisor) firstHalf
+-- input = 36_000_000
+
+houseScores :: Array Int Int
+houseScores =
+  accumArray
+    (+)
+    0
+    (1, input)
+    [ (house, elf)
+    | elf <- [1 .. input],
+      house <- [elf, elf * 2 .. input]
+    ]
+
+-- allFactors :: Int -> [Int]
+-- allFactors i = nub $ firstHalf ++ lastHalf
+--   where
+--     isqrt = floor . sqrt . fromIntegral
+--     firstHalf = filter (\divisor -> i `mod` divisor == 0) [1 .. isqrt i]
+--     lastHalf = map (\divisor -> i `div` divisor) firstHalf
 
 -- allFactors i = filter f [1 .. i]
 --   where
 --     f divisor = i `mod` divisor == 0
 
-houseScore :: Int -> Int
-houseScore i = sum $ map (* 10) (allFactors i)
+-- houseScore :: Int -> Int
+-- houseScore i = sum $ map (* 10) (allFactors i)
 
-houseScoreCum :: [Int]
-houseScoreCum = scanl1 (+) $ map houseScore [1 ..]
+-- houseScoreCum :: [Int]
+-- houseScoreCum = scanl1 (+) $ map houseScore [1 ..]
 
 part1 :: Int
-part1 = (+ 1) $ length $ takeWhile (< input) (map houseScore [1 ..])
+part1 = (+ 1) $ length $ takeWhile (\(_, v) -> v < input) $ assocs houseScores
+
+-- part1 = (+ 1) $ length $ takeWhile (< input) (map houseScore [1 ..])
