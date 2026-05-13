f9681b4bf8d61346ca5b87134f64895d7e35c627
MARUYAMA Takahito <takahito.maruyama@mixi.co.jp>
2026-05-13 20:49:54 +0900

    working on day22

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,5 +1,31 @@
 module Day22 where
+import Data.Maybe (maybeToList, listToMaybe, catMaybes)
 
 main :: IO ()
 main = do
   return ()
+
+data Turn = Player | Boss
+
+data GameState = GameState
+  { turn :: Turn,
+    manaConsumed :: Int,
+    playerHp :: Int,
+    bossHp :: Int,
+    manaRemaining :: Int,
+    turnForShield :: Int,
+    turnForPoison :: Int,
+    turnForRecharge :: Int
+  }
+
+search :: GameState -> Maybe Int
+search gs =
+  if bossDied
+    then Just (manaConsumed gs)
+    else listToMaybe $ minimum $ catMaybes $ map search nextGs
+  where
+    applyEffect = undefined
+    applied = applyEffect gs
+    bossDied = bossHp applied <= 1
+    nextGs :: [GameState]
+    nextGs = undefined
