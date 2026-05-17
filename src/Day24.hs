2efad4608b53adce35a668d74431c858098fedcc
remio87 <82135212+remio87@users.noreply.github.com>
2026-05-17 18:21:03 +0900

    adopt AI solution

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,49 +1,43 @@
 module Day24 where
 
-import Data.List (sort)
+import Data.List (minimumBy, sort)
+import Data.Ord (comparing)
 
 main :: IO ()
 main = do
   input <- readFile "inputs/day24.txt"
-  let inputL = sort $ map read $ lines input
-  putStrLn $ show inputL
+  let inputL = reverse $ sort $ map read $ lines input
   putStrLn $ "part1: " ++ show (part1 inputL)
 
 testInput :: [Int]
 testInput = [11, 10, 9, 8, 7, 5, 4, 3, 2, 1]
 
--- get all candidates
--- list is sorted and starts with largest number
--- inputs: list of Int, target value
--- output: list of candidates, which is list of Int
-allCandidates :: [Int] -> Int -> [[Int]]
-allCandidates [] 0 = [[]]
-allCandidates [] _ = []
-allCandidates (x : xs) target
-  | x == target = [[x]] ++ allCandidates xs target
-  | x > target = allCandidates xs target
-  | x < target = (allCandidates xs target) ++ (map (++ [x]) (allCandidates xs (target - x)))
-  | otherwise = undefined
+choose :: Int -> [a] -> [[a]]
+choose 0 _ = [[]]
+choose _ [] = []
+choose k (x : xs) = map (x :) (choose (k - 1) xs) ++ choose k xs
 
--- input: input list, target weight, group1 list
--- output: can compose group2 and group3
 canComposeGroups :: [Int] -> Int -> [Int] -> Bool
 canComposeGroups input target g1 = not $ null $ allCandidates remaining target
   where
     remaining = filter (`notElem` g1) input
 
+allCandidates :: [Int] -> Int -> [[Int]]
+allCandidates [] 0 = [[]]
+allCandidates [] _ = []
+allCandidates (x : xs) target
+  | x > target = allCandidates xs target
+  | x == target = [x] : allCandidates xs target
+  | otherwise = allCandidates xs target ++ map (x :) (allCandidates xs (target - x))
+
 possibleGroup1 :: [Int] -> [[Int]]
-possibleGroup1 input = filter (canComposeGroups input target) (allCandidates input target)
+possibleGroup1 input = head $ filter (not . null) $ map validOfSize [1 ..]
   where
     target = sum input `div` 3
-
-getFewestElements :: [[Int]] -> [[Int]]
-getFewestElements xs = filter (\l -> length l == minLen) xs
-  where
-    minLen = minimum $ map length xs
-
-getMinEntanglement :: [[Int]] -> Int
-getMinEntanglement xs = head $ sort $ map product xs
+    validOfSize k =
+      filter (canComposeGroups input target) $
+        filter (\g -> sum g == target) $
+          choose k input
 
 part1 :: [Int] -> Int
-part1 = getMinEntanglement . getFewestElements . possibleGroup1
+part1 input = product $ minimumBy (comparing product) $ possibleGroup1 input
